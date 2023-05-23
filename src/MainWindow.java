fbf812785e5e92887d66bab81a8a3fefc364c992
Renutte <danizimerga@gmail.com>
2023-05-23 18:03:27 +0200

    Ventana

---
+++ src/MainWindow.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    private JPanel panel1;
+    private JLabel title;
+    public MainWindow(){
+        setContentPane(panel1);
+    }
+}
